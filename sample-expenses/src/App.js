9d57bea74856d3535f6404f74b0cd21c46e2f935
Taner Çelik <taner.celik@softtech.com.tr>
2021-11-22 16:05:54 +0300

    React.createElement
     React.createElement implementation sample

--- sample-expenses/src/App.js
+++ sample-expenses/src/App.js
@@ -1,4 +1,5 @@
 
+import React from 'react';
 import Expenses from './components/Expenses';
 function App() {
   const expenses = [
@@ -22,6 +23,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  return React.createElement('div', 
+      {}, 
+      React.createElement('h2', {}, 'Let\'s get started!'),
+      React.createElement(Expenses, {item:expenses},'')
+    )
 
   return (
     <div>
